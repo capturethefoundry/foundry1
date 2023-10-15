f9f20679e3c842f62e520cbeb21945843c8876d9
cryptostaker2 <nftstaker@gmail.com>
2023-10-15 15:40:49 +0800

    Update Module 10 Typo

--- test/module10.sol
+++ test/module10.sol
@@ -3,9 +3,9 @@
 // Module 10: Assertions?
 
 // Task 1: Copy the code from Module 8.
-// Task 2: Assert that address 1 has 6 ether left.
-// Task 3: Assert that address 2 has 4 ether left.
-// Task 4: Make sure address 1 has more ether than address 2
+// Task 2: Assert that address 1 has 4 ether left after transferring address 2 6 ether.
+// Task 3: Assert that address 2 has 6 ether.
+// Task 4: Make sure address 2 has more ether than address 1
 
 // Reference: https://book.getfoundry.sh/reference/forge-std/assertEq?highlight=asserteq#asserteq
 // Reference: https://book.getfoundry.sh/reference/ds-test?highlight=assertgt#assertgt
